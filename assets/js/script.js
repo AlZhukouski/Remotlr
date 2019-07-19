a0f686df654be128c24c53e5812e33685bfd6c4f
alzhu <al.zhukouski@gmail.com>
2019-07-19 10:55:34 +0300

    Начало работы, первые блоки хеадер и бэкграунд свг который растягивается взависимости от контента

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,51 +38,6 @@ $(document).ready(function () {
     });
   });
 });
-jQuery(document).ready(function ($) {
-  var tabs = $('.tabs');
-  tabs.each(function () {
-    var tab = $(this),
-        tabItems = tab.find('ul.tabs__navigation'),
-        tabContentWrapper = tab.children('ul.tabs__content'),
-        tabNavigation = tab.find('nav');
-    tabItems.on('click', 'a', function (event) {
-      event.preventDefault();
-      var selectedItem = $(this);
-
-      if (!selectedItem.hasClass('selected')) {
-        var selectedTab = selectedItem.data('content'),
-            selectedContent = tabContentWrapper.find('li[data-content="' + selectedTab + '"]'),
-            selectedContentHeight = selectedContent.innerHeight();
-        tabItems.find('a.selected').removeClass('selected');
-        selectedItem.addClass('selected');
-        selectedContent.addClass('selected').siblings('li').removeClass('selected');
-      }
-    }); //hide the .tabs::after element when tabbed navigation has scrolled to the end (mobile version)
-
-    checkScrolling(tabNavigation);
-    tabNavigation.on('scroll', function () {
-      checkScrolling($(this));
-    });
-  });
-  $(window).on('resize', function () {
-    tabs.each(function () {
-      var tab = $(this);
-      checkScrolling(tab.find('nav'));
-      tab.find('.tabs__content').css('height', 'auto');
-    });
-  });
-
-  function checkScrolling(tabs) {
-    var totalTabWidth = parseInt(tabs.children('.tabs__navigation').width()),
-        tabsViewport = parseInt(tabs.width());
-
-    if (tabs.scrollLeft() >= totalTabWidth - tabsViewport) {
-      tabs.parent('.tabs').addClass('is-ended');
-    } else {
-      tabs.parent('.tabs').removeClass('is-ended');
-    }
-  }
-});
 /*! Magnific Popup - v1.1.0 - 2016-02-20
  * http://dimsemenov.com/plugins/magnific-popup/
  * Copyright (c) 2016 Dmitry Semenov; */
@@ -1874,6 +1829,51 @@ $(document).ready(function () {
 
   function closePopup() {
     $.magnificPopup.close();
+  }
+});
+jQuery(document).ready(function ($) {
+  var tabs = $('.tabs');
+  tabs.each(function () {
+    var tab = $(this),
+        tabItems = tab.find('ul.tabs__navigation'),
+        tabContentWrapper = tab.children('ul.tabs__content'),
+        tabNavigation = tab.find('nav');
+    tabItems.on('click', 'a', function (event) {
+      event.preventDefault();
+      var selectedItem = $(this);
+
+      if (!selectedItem.hasClass('selected')) {
+        var selectedTab = selectedItem.data('content'),
+            selectedContent = tabContentWrapper.find('li[data-content="' + selectedTab + '"]'),
+            selectedContentHeight = selectedContent.innerHeight();
+        tabItems.find('a.selected').removeClass('selected');
+        selectedItem.addClass('selected');
+        selectedContent.addClass('selected').siblings('li').removeClass('selected');
+      }
+    }); //hide the .tabs::after element when tabbed navigation has scrolled to the end (mobile version)
+
+    checkScrolling(tabNavigation);
+    tabNavigation.on('scroll', function () {
+      checkScrolling($(this));
+    });
+  });
+  $(window).on('resize', function () {
+    tabs.each(function () {
+      var tab = $(this);
+      checkScrolling(tab.find('nav'));
+      tab.find('.tabs__content').css('height', 'auto');
+    });
+  });
+
+  function checkScrolling(tabs) {
+    var totalTabWidth = parseInt(tabs.children('.tabs__navigation').width()),
+        tabsViewport = parseInt(tabs.width());
+
+    if (tabs.scrollLeft() >= totalTabWidth - tabsViewport) {
+      tabs.parent('.tabs').addClass('is-ended');
+    } else {
+      tabs.parent('.tabs').removeClass('is-ended');
+    }
   }
 }); // needs slick.js
 

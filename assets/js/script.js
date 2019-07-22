1c464c3523ff930b71f61e386575ebf3617e0ada
alzhu <al.zhukouski@gmail.com>
2019-07-22 17:00:57 +0300

    верстка блоки слайдер начало футера

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,36 @@ $(document).ready(function () {
     });
   });
 });
+$('.news__content').slick({
+  dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  responsive: [{
+    breakpoint: 1350,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3
+    }
+  }, {
+    breakpoint: 1050,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  }, {
+    breakpoint: 670,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    } // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+
+  }]
+});
 /*! Magnific Popup - v1.1.0 - 2016-02-20
  * http://dimsemenov.com/plugins/magnific-popup/
  * Copyright (c) 2016 Dmitry Semenov; */
